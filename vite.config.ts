070a4d824b18b814504f9d4416dc69909fa81dc6
Wes Bos <wesbos@gmail.com>
2025-08-18 15:24:07 -0400

    Update project configuration for ES2022, enable automatic JSX runtime, and clean up styles. Refactor imports in admin route for consistency.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic'
+  })],
   server: {
     port: 5556,
     host: true,
